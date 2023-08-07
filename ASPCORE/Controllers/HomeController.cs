68959bb037ccce6d2ffa423e69cdea606731aafd
OneGulshan <gulshankumar.mailid01@gmail.com>
2023-08-07 22:56:35 +0530

    Changing

--- ASPCORE/Controllers/HomeController.cs
+++ ASPCORE/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using ASPCORE.Infrastructure;
 using ASPCORE.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using System.Diagnostics;
 
 namespace ASPCORE.Controllers
 {
+    [Route("[controller]")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -14,8 +16,9 @@ namespace ASPCORE.Controllers
         private readonly IScopedService _scs2;
         private readonly ISingletonService _sis1;
         private readonly ISingletonService _sis2;
-
-        public HomeController(ILogger<HomeController> logger, ITransientService ts1, ITransientService ts2, IScopedService scs1, IScopedService scs2, ISingletonService sis1, ISingletonService sis2)
+        private readonly IMemoryCache _memoryCache;
+        public HomeController(ILogger<HomeController> logger, ITransientService ts1, ITransientService ts2, IScopedService scs1, IScopedService scs2, ISingletonService sis1, ISingletonService sis2
+            , IMemoryCache memoryCache)
         {
             _logger = logger;
             _ts1 = ts1;
@@ -24,9 +27,11 @@ namespace ASPCORE.Controllers
             _scs2 = scs2;
             _sis1 = sis1;
             _sis2 = sis2;
+            _memoryCache = memoryCache;
         }
 
-        public IActionResult Index()
+        [Route("[action]")]
+        public IActionResult DependencyIndex()
         {
             ViewBag.ts1 = _ts1.GetTaskID().ToString();
             ViewBag.ts2 = _ts2.GetTaskID().ToString();
@@ -37,9 +42,35 @@ namespace ASPCORE.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [Route("[action]")]
+        public IActionResult CachingIndex()
+        {
+            DateTime CurrentTime;
+            bool value = _memoryCache.TryGetValue("CachedTime", out CurrentTime);
+            if (!value)
+            {
+                CurrentTime = DateTime.Now;
+                var cachedEntryOption = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(10));
+                _memoryCache.Set("CachedTime", CurrentTime, cachedEntryOption);
+            }
+            return View(CurrentTime);
+        }
+
+        [Route("[action]")]
+        public IActionResult QueryStringIndex()
         {
             return View();
+        }
+        [Route("[action]")]
+        public IActionResult GetString(string first)
+        {
+            return View();
+        }
+        [Route("[action]")]
+        public IActionResult PersonIndex()
+        {
+            Person persons = new() { PersonId = 1, PersonName = "Gulshan", Age = 27 };
+            return View(persons);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
